8af95942153a3027807753a99971cee1484f9bc6
安兆全 <603257688@qq.com>
2020-02-18 20:30:01 +0800

    第一次修改

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import axios from 'axios'
 
 Vue.config.productionTip = false
 
@@ -10,3 +11,10 @@ new Vue({
   store,
   render: h => h(App)
 }).$mount('#app')
+
+axios({
+  url:'https://api.apiopen.top/musicRankings',
+}).then( res=>{
+  console.log(res)
+  console.log(res.data.result[5].name)
+})
